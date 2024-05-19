06c0c783668945f1c524d7725cf3a2409ab99db7
revanth892 <revanth9347569169@gmail.com>
2024-05-19 23:51:07 +0530

    practicing redis

--- string.js
+++ string.js
@@ -6,7 +6,7 @@ async function init()
     const category ="asia";
     // const country="india"
     let series =1
-    const data = {"capital":"delhi"};
+    const data = {"capital":"hyd"};
     const results = JSON.stringify(data)
     // await client.setnx(`bikes:${category}:${series}` ,results);
     // await client.set("user:2" ,"annulu");
@@ -14,8 +14,10 @@ async function init()
     // const result = await client.get("user:1")
     // console.log(result)
     // console.log("Result ->",result)
-    const d1= await client.get(`bikes:${category}:${series}`);
-    console.log(JSON.parse(d1))
+    let d1= await client.get(`bikes:${category}:${series}`);
+    d1=JSON.parse(d1)
+    console.log(d1)
+    console.log(d1.capital)
 
 }
 
